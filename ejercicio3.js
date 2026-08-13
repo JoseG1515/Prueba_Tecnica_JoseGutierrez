03f0a61a1da9e866665152bc8eb4ffbfd5d91490
JoseG1515 <jg387782@gmail.com>
2026-08-12 22:18:05 -0500

    ejercicio3.js
    desarrollé el ejercicio de comprimir texto.

---
+++ ejercicio3.js
@@ -0,0 +1,26 @@
+//Ejercicio 3 - comprimir texto
+let textos= 'aaabbbcccd'
+
+function ComprimirTexto(textos){
+    let palabra = textos.toLowerCase()
+    console.log(palabra)
+    let resultado = ''
+    let cont= 1
+if (palabra.length==0){
+    resultado =''
+    return resultado
+}else { 
+    for (let i=0; i<palabra.length; i++){
+        //resultado += palabra[i]
+         if (palabra[i] == palabra[i+1]){
+cont +=1
+         }else{
+            resultado += palabra[i]+ cont
+            cont = 1 
+         }
+    }
+}
+
+ return resultado
+}
+ console.log(ComprimirTexto(textos))
